0247f2c3cc9c6ed896d4837ea14e7b6f7cab0148
Konrad Blum <konrad.blum@gmail.com>
2014-01-12 19:04:47 +0200

    Get eBucks balance.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-ebucks
-======
+# eBucks
 
-Python module for scraping balance from eBucks website.
+Python module for scraping balance from [eBucks](https://www.ebucks.com) website.
+
+
+## Usage
+
+    from ebucks import get_balance
+    username = 'USERNAME'  # your eBucks username
+    password = 'PASSWORD'  # your eBucks password
+    balance = get_balance(username, password)
+    print 'eBucks balance: eB {0}'.format(balance)
---
+++ ebucks/balance.py
@@ -0,0 +1,31 @@
+import urllib, urllib2, cookielib
+from BeautifulSoup import BeautifulSoup
+import re
+
+
+def get_balance(username, password):
+    """
+    Scrape eBucks website to get balance and return as an integer.
+    """
+
+    # create opener
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+    urllib2.install_opener(opener)
+
+    # post to login validation page
+    login_url = 'https://www.ebucks.com/web/loginValidate.do?Action=Login'
+    login_data = {'userId': username, 'password': password}
+    opener.open(login_url, urllib.urlencode(login_data)).close()
+
+    # get contents of home page
+    home_url = 'https://www.ebucks.com/web/eBucks/'
+    fp = opener.open(home_url)
+    page = fp.read()
+    fp.close()
+
+    # parse contents of home page to get the balance
+    soup = BeautifulSoup(page)
+    balance_str = soup.find('span', {'class': 'eBucksValue'}).text.__str__()
+    balance = int(re.findall('([0-9]+)', balance_str)[0])
+    
+    return balance
